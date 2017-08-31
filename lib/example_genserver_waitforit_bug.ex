fae9331b9ffc719aa1a19c599e2dad444dbe9f29
James Smith <james@gaslight.co>
2017-08-30 21:35:13 -0400

    Demonstrates bug

--- lib/example_genserver_waitforit_bug.ex
+++ lib/example_genserver_waitforit_bug.ex
@@ -1,18 +1,23 @@
 defmodule ExampleGenserverWaitforitBug do
+  use GenServer
+  require WaitForIt
   @moduledoc """
   Documentation for ExampleGenserverWaitforitBug.
   """
 
-  @doc """
-  Hello world.
+  def start_link() do
+    names = ["world"]
+    GenServer.start_link(__MODULE__, names, name: __MODULE__)
+  end
 
-  ## Examples
+  def hello(name) do
+    GenServer.call(__MODULE__, {:request_greeting, name})
+  end
 
-      iex> ExampleGenserverWaitforitBug.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def handle_call({:request_greeting, name}, _from, names) do
+    greeting = WaitForIt.case_wait name do
+      "world" -> "Hello world"
+    end
+    {:reply, greeting, names}
   end
 end
